bb7edb281365e5787f6d2fb3e34cf964536028c9
rdowd <rjarrettdowd@yahoo.com>
2020-07-14 19:25:53 -0400

    Completed single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,21 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    #first pass solution
+    single_num = 0
+    #get the largest number in the array and add 1
+    max_num = max(arr) + 1
+    #create a count array of zeros the size of max number in arr
+    count_arr = [0] * max_num 
+    #for every item in arr check its value and increment count array +1 at index of that value
+    for i in arr:
+        count_arr[i] += 1
+    #loop through count array if value is equal to 1 return that index
+    for i in range(len(count_arr)):
+        if count_arr[i] == 1:
+            single_num = i
 
-    pass
+    return single_num
 
 
 if __name__ == '__main__':
